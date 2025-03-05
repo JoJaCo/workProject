5cb5bf5216ad30ef738295ad5a828230eb1388d9
JoJaCo <contrerasjorge226@gmail.com>
2025-03-05 03:48:48 -0800

    created the checkUserInput fucntion to check users info with the Data base

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,34 @@
 fun main(args: Array<String>) {
-    println("please enter your user name: ")
-    val name = readLine()?.trim() ?: ""
-    println("Please enter your user id(4325):")
-    val userId = readLine()?.toIntOrNull() ?:""
 
-    println(name)
-    println(userId)
+   val staff = employees
+
+    while (true) {
+
+        println("please enter your user name: ")
+        val name = readLine()?.trim() ?: ""
+
+        println("Please enter your user id(4325):")
+        val userId = readLine()?.trim() ?: ""
+        val userNum = userId.toInt()
+
+        println(name)
+        println(userId)
+
+        //sending the info to check with the database
+        val isInDataBase = checkUserInput(name, userNum, staff)
+
+        if (isInDataBase) {
+            println("Match Found")
+        } else {
+            println("Match not found")
+        }
+    }
 
 
 }
 
-fun checkUserInput(){
+fun checkUserInput(name: String, id:Int, employees: List<Users>): Boolean {
+    return employees.contains(Users(name, id))
 
 
 }
