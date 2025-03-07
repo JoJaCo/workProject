db78013264f4c9bebd91e09287a8e38f42c95aeb
JoJaCo <contrerasjorge226@gmail.com>
2025-03-07 02:11:40 -0800

    Created a callout function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,92 @@
+
 fun main(args: Array<String>) {
 
-   val staff = employees
+    Ui.start()
 
-    while (true) {
+}
 
-        println("please enter your user name: ")
-        val name = readLine()?.trim() ?: ""
 
-        println("Please enter your user id(4325):")
-        val userId = readLine()?.trim() ?: ""
-        val userNum = userId.toInt()
 
-        println(name)
-        println(userId)
+object Ui{
+    // TODO: we will move everthing in the main inot this section and continue
 
-        //sending the info to check with the database
-        val isInDataBase = checkUserInput(name, userNum, staff)
+    val staff = employees
+    var keepChecking = true
 
-        if (isInDataBase) {
-            println("Match Found")
-        } else {
-            println("Match not found")
+
+    init {
+        println("************************")
+        println("Welcome,H & R employee")
+        println("************************")
+    }
+
+    fun start() {
+        while (keepChecking) {
+
+            println()
+            println("please enter your user name: ")
+            val name = readLine()?.trim() ?: ""
+
+            println()
+            println("Please enter your user id(4325):")
+            val userId = readLine()?.trim() ?: ""
+            val userNum = userId.toInt()
+
+
+            //sending the info to check with the database
+            val isInDataBase = checkUserInput(name, userNum, staff)
+
+            //result from checking the database
+            if (isInDataBase) {
+                println("Welcome ${name}, hope your having a good day ")
+                commands()
+            } else {
+                println("Match not found")
+
+            }
+
+
+
+            println("Would you like to keep checking the database: yes or no  ")
+            val keepGoing = readlnOrNull() ?: "Invalid answer"
+            keepChecking = keepGoing == "yes"
+
         }
+        /*the end of our while loop*/
+
+
+    }
+    private fun commands() : String {
+
+        val result = when(callOutListOption()){
+            "1" -> "Call out due to being sick "
+            "2" -> "Call out due to being sick "
+            "3" -> "Call out due to being sick "
+            "4" -> "Call out due to being sick "
+            else -> "Invalid"
+        }
+
+        return result
+    }
+
+
+
+
+    private fun callOutListOption(): String? {
+
+        println("Here are your list of options for call outs: ")
+        println("1: Calling out due to being Sick ")
+        println("2: Calling out due to Doctors appointment ")
+        println("3: Calling out due to Personal matters ")
+        println("4: other ")
+        println("Please input 1,2 or 3 depending on your callout ")
+        return readLine()?.trim() ?:""
+
     }
 
 
 }
+
 
 fun checkUserInput(name: String, id:Int, employees: List<Users>): Boolean {
     return employees.contains(Users(name, id))
